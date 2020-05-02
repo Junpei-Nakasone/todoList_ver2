56686548702ad425b2f7c42152e4d90c78596807
Junpei-Nakasone <nakasone.junpei0719@gmail.com>
2020-05-02 16:22:51 +0900

    modified

---
+++ api/api004/domain/model.go
@@ -0,0 +1,12 @@
+package domain
+
+// RequestParam receives id
+type RequestParam struct {
+	ID int `json:"id"`
+}
+
+// Todo id containing todo
+type Todo struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
--- environment/router/router.go
+++ environment/router/router.go
@@ -4,6 +4,7 @@ import (
 	"github.com/labstack/echo"
 	api001 "github.com/todoList_ver2/api/api001/handler"
 	api002 "github.com/todoList_ver2/api/api002/handler"
+	api003 "github.com/todoList_ver2/api/api003/handler"
 )
 
 // NewRouter returns router
@@ -13,6 +14,8 @@ func NewRouter() *echo.Echo {
 	e.File("/", "public/index.html")
 	e.GET("/todos", api001.ShowTodos)
 	e.POST("/addTodo", api002.AddTodo)
+	e.POST("/deleteTodo", api003.DeleteTodo)
+	// e.POST("updateTodo", )
 
 	return e
 }
